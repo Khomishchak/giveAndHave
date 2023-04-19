5737bb0de4989a893a449b8f25c2ace63e33301a
Maksym Khomishchak <mkhomishchak@amelco.co.uk>
2023-04-19 13:57:50 +0100

    GIV-6 Check if user applied for its own task

--- src/main/java/com/khomishchak/giveAndHave/service/TaskServiceImpl.java
+++ src/main/java/com/khomishchak/giveAndHave/service/TaskServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class TaskServiceImpl implements TaskService{
@@ -41,11 +42,13 @@ public class TaskServiceImpl implements TaskService{
     @Override
     public void postRequestForTask(Long userId, Long taskId) {
 
-        User user = userRepository.findById(userId).orElseThrow(
-                () -> new IllegalArgumentException()
-        );
+        User user = findUserByIdOrThrowException(userId);
 
         Task task = findTaskOrThrow(taskId);
+
+        if(Objects.equals(userId, task.getUser().getId())) {
+            return;
+        }
 
         Application application = Application.builder()
                 .user(user)
@@ -57,7 +60,13 @@ public class TaskServiceImpl implements TaskService{
 
     private Task findTaskOrThrow(Long id) {
 
-        return taskRepository.findById(id).
-            orElseThrow(() -> new IllegalArgumentException());
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found!"));
+    }
+
+    private User findUserByIdOrThrowException(Long id) {
+
+        return userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User not found!"));
     }
 }
